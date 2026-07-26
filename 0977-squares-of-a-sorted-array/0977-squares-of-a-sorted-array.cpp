467df4fdc35a427cbb5aedaeb34247940af12d67
adityakumar712 <153096631+adityakumar712@users.noreply.github.com>
2026-07-26 16:25:25 +0530

    Time: 3 ms (51.62%), Space: 32 MB (8.29%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,82 +1,73 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
+       vector<int>pos;
+       vector<int>neg;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] >= 0){
-                pos.push_back(nums[i]);
-            }
-            else{
-                neg.push_back(nums[i]);
-            }
+       for(int i=0; i<nums.size(); i++){
+        if(nums[i] >= 0){
+            pos.push_back(nums[i]);
         }
-
-        if(pos.size() == 0){
-            for(int i=0; i<neg.size(); i++){
-                neg[i] = neg[i] * neg[i];
-            }
-
-            reverse(neg.begin() , neg.end());
-            return neg;
+        else{
+            neg.push_back(nums[i]);
         }
+       }
 
-        if(neg.size() == 0){
-            for(int i=0; i<pos.size(); i++){
-                pos[i] = pos[i] * pos[i];
-            }
-
-            return pos;
-        }
-
+       if(neg.size() == 0){
         for(int i=0; i<pos.size(); i++){
-                pos[i] = pos[i] * pos[i];
-            }
-
-        for(int i=0; i<neg.size(); i++){
-                neg[i] = neg[i] * neg[i];
-            }
-
-         reverse(neg.begin() , neg.end());
-
-
-        int i=0;
-        int j=0;
-        int index = 0;
-        vector<int>res(pos.size()+neg.size());
-
-        while(i< pos.size() and j< neg.size()){
-            if(pos[i] <= neg[j]){
-                res[index] = pos[i];
-                index++;
-                i++;
-            }
-            else{
-                res[index]=neg[j];
-                index++;
-                j++;
-
-            }
+            pos[i] = pos[i]*pos[i];
         }
+        return pos;
+       }
 
-        while(j < neg.size()){
-            res[index] = neg[j];
+       if(pos.size() == 0){
+        for(int i=0; i<neg.size(); i++){
+            neg[i]= neg[i]*neg[i];
+        }
+        reverse(neg.begin() , neg.end());
+        return neg;
+       }
+
+       for(int i=0; i<pos.size(); i++){
+        pos[i]=pos[i]*pos[i];
+       }
+
+       for(int i=0; i<neg.size(); i++){
+        neg[i]=neg[i]*neg[i];
+       }
+
+       reverse(neg.begin() , neg.end());
+
+       int i=0;
+       int j=0;
+       int index =0;
+       vector<int>res(pos.size() + neg.size());
+
+       while(i<pos.size() and j<neg.size()){
+        if(pos[i] <= neg[j] ){
+            res[index]=pos[i];
+            index++;
+            i++;
+        }
+        else{
+            res[index]=neg[j];
             index++;
             j++;
         }
+       }
 
-        while(i < pos.size()){
-            res[index] = pos[i];
-            index++;
-            i++;
+       while(i < pos.size()){
+        res[index] = pos[i];
+        index++;
+        i++;
+       }
 
-        }
+       while(j < neg.size()){
+        res[index] = neg[j];
+        index++;
+        j++;
+       }
 
-        for(int k=0; k<res.size(); k++){
-            nums[k] = res[k];
-        }
-
-        return nums;
+       return res;
     }
 };
