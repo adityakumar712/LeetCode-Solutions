7fcd34dd5022755757e13d8cbf501e8155e4ae03
adityakumar712 <153096631+adityakumar712@users.noreply.github.com>
2026-09-03 00:56:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.62%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+
+        for(int stone : stones){
+            pq.push(stone);
+        }
+
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+
+            if(x==y){
+                continue;
+            }
+
+            if(x!=y){
+                y = y - x;
+                pq.push(y);
+            }
+        }
+
+        if(pq.size()==0){
+            return 0;
+        }
+
+        return pq.top();
+
+
+    }
+};
