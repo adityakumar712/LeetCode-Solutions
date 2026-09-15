7b9dba17c8fe48b0f8f5b45bf7ca2f7b14933993
adityakumar712 <153096631+adityakumar712@users.noreply.github.com>
2026-09-15 23:59:07 +0530

    Time: 3 ms (51.22%), Space: 18.2 MB (18.91%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+   void fun(vector<int>& candidates , int n , int idx , vector<int>&diary , int sum , vector<vector<int>>&res , int target){
+       if(idx == n){
+        if(sum == target){
+            res.push_back(diary);
+        }
+            return;
+        }
+
+        fun(candidates , n , idx+1 , diary , sum , res ,target);
+
+        if(candidates[idx]+sum <= target){
+            diary.push_back(candidates[idx]);
+            sum += candidates[idx];
+
+            fun(candidates , n , idx , diary , sum , res ,target);
+            diary.pop_back();
+            sum -= candidates[idx];
+
+        }       
+   }
+
+
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        int idx = 0;
+        int sum =0;
+        vector<int>diary;
+        vector<vector<int>>res;
+
+        fun(candidates , n , idx , diary , sum , res , target);
+        return res;
+    }
+};
