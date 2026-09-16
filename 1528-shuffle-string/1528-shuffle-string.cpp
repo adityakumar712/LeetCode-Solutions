5b9abbc526344d4961a5813673177d130fc56841
adityakumar712 <153096631+adityakumar712@users.noreply.github.com>
2026-09-16 22:42:59 +0530

    Time: 7 ms (4.23%), Space: 20.5 MB (5.46%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -4,11 +4,11 @@ public:
         int n = indices.size();
         string str ="";
         vector<string>v(n);
-        for(int i=0; i<indices.size(); i++){
+        for(int i=0; i<n; i++){
             v[indices[i]].push_back(s[i]);
         }
 
-        for(int i=0; i<v.size(); i++){
+        for(int i=0; i<n; i++){
             str+=v[i];
         }
 
